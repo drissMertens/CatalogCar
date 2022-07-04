2a665b88c003089d9f49265c86f7a8c985d790e7
Driss Chennaoui <driss.chennaoui@altimet.fr>
2022-07-04 10:23:25 +0200

    first commit

---
+++ locationCar/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+//class catalog
+
+public abstract class Automobile 
+{
+    protected string modele;
+    protected string couleur;
+    protected int puissance;
+    protected double espace;
+
+    public Automobile(string modele, string couleur, int puissance, double espace)
+    {
+        this.modele = modele;
+        this.couleur = couleur;
+        this.puissance = puissance;
+        this.espace = espace;
+    }
+
+    public abstract void afficherCarac();
+
+}
